fcaa4bd365b7f41ba4d0149f1e3806706614ff3e
Diego Gomez <diego.gomez2005@gmail.com>
2016-04-15 14:51:10 -0300

    added authentication and signup methods

--- model/user.js
+++ model/user.js
@@ -13,6 +13,10 @@ var UserSchema = new Schema({
     password: {
         type: String,
         required: true
+    },
+    email: {
+        type: String,
+        required: true
     }
 });
 
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,53 @@
 var express       = require('express');
 var router        = express.Router();
 var passport	  = require('passport');
+var User          = require('../model/user');
+var jwt           = require('jwt-simple');
+var config        = require('../config');
 
 router.get('/', requiresAuthentication(), function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.post('/signup', function(req, res) {
+  if (!req.body.name || !req.body.password) {
+    res.json({success: false, msg: 'Missed name or password.'});
+  } else {
+    var newUser = new User({
+      name: req.body.name,
+      password: req.body.password,
+      email: req.body.email
+    });
+
+    newUser.save(function(err) {
+      if (err) {
+        return res.json({success: false, msg: 'Username already exists.'});
+      }
+      res.json({success: true, msg: 'Successful created new user.'});
+    });
+  }
+});
+
+router.post('/authenticate', function(req, res) {
+  User.findOne({
+    name: req.body.name
+  }, function(err, user) {
+    if (err) throw err;
+
+    if (!user) {
+      res.send({success: false, msg: 'Authentication failed. User not found.'});
+    } else {
+      // check if password matches
+      user.comparePassword(req.body.password, function (err, isMatch) {
+        if (isMatch && !err) {
+          var token = jwt.encode(user, config.secretOrKey);
+          res.json({success: true, token: 'JWT ' + token});
+        } else {
+          res.send({success: false, msg: 'Authentication failed. Wrong password.'});
+        }
+      });
+    }
+  });
 });
 
 function requiresAuthentication(){
